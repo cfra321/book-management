9cac1bc2faa1a2b0744a75427c8ee558ea394700
kukuh-wicaksono <kukuh.wicaksono@amarbank.co.id>
2024-09-07 12:48:27 +0700

    update config for deploy

--- main.go
+++ main.go
@@ -29,11 +29,11 @@ func main() {
 	}
 
 	psqlInfo := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
 	)
 
 	DB, err = sql.Open("postgres", psqlInfo)
@@ -46,11 +46,11 @@ func main() {
 	database.DBMigrate(DB)
 
 	psqlSeeder := fmt.Sprintf(`host=%s port=%s user=%s password=%s dbname=%s sslmode=disable`,
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
 	)
 
 	DB, err = sql.Open("postgres", psqlSeeder)
@@ -88,5 +88,5 @@ func main() {
 		authorized.DELETE("/user/:id", controllers.DeleteUser)
 	}
 
-	router.Run(":8080")
+	router.Run(":" + os.Getenv("PORT"))
 }
